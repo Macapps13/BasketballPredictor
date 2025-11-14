1c32bac5a545fdade234000c79b933f56e8211be
Ben Alvaro <ben.alvaro@icloud.com>
2025-11-14 13:07:30 +1100

    feat: added the ability to view odds for all games

--- outcome.py
+++ outcome.py
@@ -39,9 +39,7 @@ def get_teams():
     return team1, team2 
 
 def findGameId(team1_id, team2_id):
-    f = "{gameId}: {awayTeam} vs. {homeTeam} @ {gameTimeLTZ}" 
-    board = scoreboard.ScoreBoard()
-    games = board.games.get_dict()
+    f = "{gameId}: {awayTeam} @ {homeTeam}  {gameTimeLTZ}" 
     game_id = None
     for game in games:
         gameTimeLTZ = parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -50,23 +48,47 @@ def findGameId(team1_id, team2_id):
             print(f.format(gameId=game['gameId'], awayTeam=game['awayTeam']['teamName'], homeTeam=game['homeTeam']['teamName'], gameTimeLTZ=gameTimeLTZ))
             print(f"Game ID: {game['gameId']}")
             game_id = game['gameId']
+            home = game['homeTeam']['teamName']
+            away = game['awayTeam']['teamName']
     if not game_id:
         print("No scheduled game found between the selected teams today.")
         exit()
-    return game_id
+    return game_id, home, away
+
+def get_game_odds(gameID, home, away):
+    odds = Odds()
+    games_list = odds.get_games().get_dict()
+    for game in games_list:
+        if game['gameId'] == gameID:
+            for market in game['markets']:
+                for book in market['books']:
+                    if book['countryCode'] == 'US' and market['name'] == '2way':
+                        print(f"Book: {book['name']}")
+                        for outcome in book['outcomes']:
+                            if outcome['type'] == 'home':
+                                outcome['type'] = home
+                            elif outcome['type'] == 'away':
+                                outcome['type'] = away
+                            print(f"{outcome['type']}: {outcome['odds']}")
 
 
-team1, team2 = get_teams()
-print(f"Team 1: {team1['full_name']}")
-print(f"Team 2: {team2['full_name']}")
-team1_id = team1['id']
-team2_id = team2['id']
-gameID = findGameId(team1_id, team2_id)
-odds = Odds()
-games_list = odds.get_games().get_dict()
-for game in games_list:
-    if game['gameId'] == gameID and game['markets']['countryCode'] == 'US':
-        print(json.dumps(game, indent=4))
+
+board = scoreboard.ScoreBoard()
+games = board.games.get_dict()
+print("See all Odds, or 1 game?")
+input = input("1: All Odds\n2: 1 Game\n")
+print("-----")
+if input == "1":
+    for game in games:
+        get_game_odds(game['gameId'], game['homeTeam']['teamName'], game['awayTeam']['teamName'])
+        print("-----")
+elif input == "2":
+    team1, team2 = get_teams()
+    print(f"Team 1: {team1['full_name']}")
+    print(f"Team 2: {team2['full_name']}")
+    team1_id = team1['id']
+    team2_id = team2['id']
+    gameID, home, away = findGameId(team1_id, team2_id)
 
 
 
