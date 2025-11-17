427d5596ed2a6a58e4b1700aae1c8097be831ed4
Ben Alvaro <ben.alvaro@icloud.com>
2025-11-17 16:42:23 +1100

    feat: added getstats file that creates the input matrix for the ML

--- getStats.py
+++ getStats.py
@@ -4,7 +4,7 @@ from nba_api.live.nba.endpoints import scoreboard
 from nba_api.live.nba.endpoints import Odds
 from nba_api.stats.endpoints import teamestimatedmetrics
 from nba_api.stats.endpoints import leaguedashteamstats
-from nba_api.stats.endpoints import teamgamelog
+from nba_api.stats.endpoints import teamgamelogs
 
 import pandas as pd
 import json
@@ -14,23 +14,36 @@ from datetime import datetime, timezone
 from dateutil import parser
 import math
 
-def get_stats(homeID, awayID):
+
+def get_stats(teamID):
     estRatings = teamestimatedmetrics.TeamEstimatedMetrics()
     stats = leaguedashteamstats.LeagueDashTeamStats()
-    gameLogHome = teamgamelog.TeamGameLog(team_id = "1610612742")
+    gameLogHome = teamgamelogs.TeamGameLogs(
+    team_id_nullable=teamID,
+    season_nullable="2025-26",
+    season_type_nullable="Regular Season"
+    )
     gameLogHome_df = gameLogHome.get_data_frames()[0]
     print(gameLogHome_df.head())
-   
+    gameLogHome_df['GAME_DATE'] = pd.to_datetime(gameLogHome_df['GAME_DATE'])
+    gameLogHome_df = gameLogHome_df.sort_values('GAME_DATE')
+    last5 = gameLogHome_df.tail(5)
+    avg_pm_last5 = last5['PLUS_MINUS'].mean()
     ratings_df = estRatings.get_data_frames()[0]
     stats_df = stats.get_data_frames()[0]
-    home_wpct = stats_df.loc[stats_df['TEAM_ID'] == int(homeID), 'W_PCT'].iloc[0]
-    away_wpct = stats_df.loc[stats_df['TEAM_ID'] == int(awayID), 'W_PCT'].iloc[0]
-    wpct_diff = home_wpct - away_wpct
-    home_net = ratings_df.loc[ratings_df['TEAM_ID'] == int(homeID), 'E_NET_RATING'].iloc[0]
-    away_net = ratings_df.loc[ratings_df['TEAM_ID'] == int(awayID), 'E_NET_RATING'].iloc[0]
-    net_diff = home_net - away_net
-    print("Win Pct diff = " + str(wpct_diff))
-    print("Net Rating diff = " + str(net_diff))
-    return 
+    wpct = stats_df.loc[stats_df['TEAM_ID'] == int(teamID), 'W_PCT'].iloc[0]
+    net = ratings_df.loc[ratings_df['TEAM_ID'] == int(teamID), 'E_NET_RATING'].iloc[0]
+    
+    print(f"Team ID: {teamID}")
+    print(f"Win Percentage: {wpct:.3f}")
+    print(f"Net Rating: {net:.3f}")
+    print(f"Average Plus/Minus Last 5 Games: {avg_pm_last5:.3f}")
+    return [wpct, net, avg_pm_last5]
 
-get_stats(1610612764, 1610612754)
+home_matrix = get_stats(1610612764)
+away_matrix = get_stats(1610612761)
+M = []
+for i in range(len(home_matrix)):
+    M.append(home_matrix[i] - away_matrix[i])
+print(M)
+
